7a9e78a15929f94b19ab4309618bc15920b7e9fe
Huong Yen Le <prohle@gmail.com>
2020-06-08 17:58:25 -0400

    highlight the ease to call a function from Julia

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
+# test.py
+# ! IMPORTANT: Do not run untill you have finished all steps in installation.md
 import julia
 from julia import SpectralGM
 
 # julia.install() # uncomment this if you run into error. Then try again. After that, in future run, you should comment this line out.
 j = julia.Julia()
+
+## Part 1
+# this is to test to call direct functions within Python code
 x = j.include("test.jl")
-
 # print(type(x)) # in case you are curious of the type
-
 hiddenf, s1, s2 = x()
-
 hiddenf(5, 5)
 
-
+## Part 2
+# this is to test to call functions from imported packages from julia.
+# we can see that we call the function just like we call a python function.
 SpectralGM.test("Huong")
 SpectralGM.greet()
 
